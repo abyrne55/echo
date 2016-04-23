ef56260e48190d35ff1c5040bd03734455bda1f3
Anthony Byrne <anthony@abyrne.me>
2016-04-22 21:50:22 -0400

    Initial commit of "main" file

---
+++ echo.py
@@ -0,0 +1,10 @@
+"""
+Boston University Rocket Propulsion Group: Echo
+Post-Rocket Engine Test Data Upload & Analysis Tool
+Created by Anthony Byrne & Silvia Zhang for ENG EK128 (Spring 2016)
+
+See LICENSE for MIT/X11 license info.
+See README for all other help.
+"""
+
+print("Welcome to BURPG Echo.")
