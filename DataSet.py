331129787ecf0b9f8274dd28f07a47de60c7676f
silvizha <silvizha@bu.edu>
2016-04-23 23:47:27 -0400

    Fixed pydoc

--- DataSet.py
+++ DataSet.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 23 02:26:51 2016
 
-@author: silvia_zhang_97
-"""
 
 class DataSet:
     def __init__(self, dataname,xlist,ylist):
         """
         A class to create object Datasets to be used for plotting. A list of these are generated, and cannot exceed 10 plots.
+        
         :param dataname: telemetry value being plotted ie VOXB
         :param xlist: list of independent values to be plotted over
         :param ylist: list of telemetry values to plot over xlist
-        
         """
         self.dataname = dataname
         self.xlist = xlist
--- plotting.py
+++ plotting.py
@@ -1,41 +1,62 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 22 21:03:14 2016
 
-@author: silvia_zhang_97
-"""
 import matplotlib.pyplot as plt
 from scipy import signal
+import os 
 class DataAnalysis:
+    interactive = False
+    LINE_COLORS = ["#de3c80","#d9013a","#d47e20","#17c009","#196512","#2a9e60","#116186"]
+    def __init__(self,interactive, location = os.getcwd()):
+        self.interactive = interactive
+        
     
-    def __init__(self,interactive):
-        interactive = False
+    def plotit(self,data,title,xlabel,ylabel):
+        """
+         plots telemetry values given in a list
+        
+         if there are multiple telemetry values, they will be in a list of lists
+         This method will work for a single or multiple plots.
+         if interactive is true, will print plot to screen as a figure
+         else, it will save the figure as a pdf using the title found in the dataset
+         
+         :param data: Either a DataSet object or a list of DataSet Objects
+         :param title: Title of the plot
+         :param xlabel: label on x axis in form "Type (units)" ie. "Time (s)"
+         :param ylabel: label on y axis in form "Type (units)" ie. "Thrust (lbs)"
+        """
+        legendnames = []
+        dslist = []
+        
+        if (len(data) > 1):
+            dslist = data
     
-    def simple_plot(title,x,y,xlabel,ylabel):
-        """
-         plots single telemetry value over time 
-         :param title: title of the plot
-         :param x: list of regularly incremented time values
-         :param y: list of telemetry values per x value
-         :param xlabel: x variable name and units ie. Time(s)
-         :param ylabel: y variable name and units ie. Thrust(lbs)
-        """
-        plt.figure();
-        plt.plot(x,y);
+        else:
+            dslist = [data]
+        for ds in dslist:
+            plt.plot(ds.xlist,ds.ylist, color = DataAnalysis.LINE_COLORS[dslist.index(ds)])
+            legendnames.append(ds.dataname)
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.grid(True)
-    
-    
-    def filter_data(y,N,Wn):
+                
+        
+        if (self.interactive == True):
+            plt.show()
+        else:
+            plt.savefig(ds.title + ".pdf")
+        
+            
+    def filter_data(self,y,N,Wn):
         """
         takes raw telemetry data and applies a butterworth filter
+        
         :param y: raw telemetry data
         :param N: order of butterworth filter
         :param Wn: cutoff frequency
+        :returns: filtered data
         """
         b,a, = signal.butter(N,Wn, output = 'ba')
         yf = signal.filtfilt(b,a, y)
         return yf
         
+            
